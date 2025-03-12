a948da73329ca8fc3e506920445ab6659bc879c6
Ishaan Datta <ishaandatta19@gmail.com>
2025-03-12 19:06:06 +0530

    Add: Basic file reading to retrieve grammar from txt, incomplete fn. defn.

--- parser.c
+++ parser.c
@@ -1,6 +1,30 @@
 #include "parser.h"
 #include "parserDef.h"
+#include <stdio.h>
 
+Grammar getGrammarFromFile(char** file_name)
+{
+	Grammar grammar;
+	FILE *fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	fp = fopen(*file_name, "r");
+	if (fp == NULL)
+		printf("Couldn't open grammar file\n");exit(1); 
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+		// printf("Retrieved line of length %zu :\n", read);
+		// printf("%s", line);
+		// TODO: Add line processing using strtok()
+	}
+
+	fclose(fp);
+	if (line)
+		free(line);
+	return grammar;
+}
 
 FirstAndFollow ComputeFirstAndFollowSet(Grammar G)
 {
