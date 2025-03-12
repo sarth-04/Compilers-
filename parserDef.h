5776726e7c425b509c0e1b2d1f560a9c8ef5beb7
Ishaan Datta <ishaandatta19@gmail.com>
2025-03-12 16:22:25 +0530

    Add: Defined types for grammar

--- parserDef.h
+++ parserDef.h
@@ -1,4 +1,4 @@
-#include "lexerDef.h" // TODO: reconcile differences with actual lexerDef.h
+#include "lexerDefDummy.h" // TODO: reconcile differences with actual lexerDef.h
 // A grammar consists of rules A -> B, where A and B are non-terminals
 typedef struct grammar 
 {
@@ -14,12 +14,14 @@ typedef struct Rule
 
 typedef struct SymbolList
 {
+	int length;
 	Symbol* symbol_list;
 } SymbolList;
 
 typedef struct Symbol
 {
-	
+	char** lexeme;
+	TokenType tok_type;
 } Symbol;
 
 typedef struct FirstAndFollow
