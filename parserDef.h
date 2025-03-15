daab824ea525c4ada41231eb48333703af33c0c4
Ishaan Datta <ishaandatta19@gmail.com>
2025-03-15 12:05:42 +0530

    Add: Include token interface definitions in parserDef itself

--- parserDef.h
+++ parserDef.h
@@ -7,6 +7,65 @@
 #define DOLLAR_TOKEN  -2 
 #define SYN_SET -3  
 
+
+typedef enum {
+	// Keywords
+	TK_WITH, TK_PARAMETERS, TK_END, TK_WHILE, TK_UNION,
+	TK_ENDUNION, TK_DEFINETYPE, TK_AS, TK_TYPE, TK_MAIN,
+	TK_GLOBAL, TK_PARAMETER, TK_LIST, TK_INPUT, TK_OUTPUT,
+	TK_INT, TK_REAL, TK_ENDWHILE, TK_IF, TK_THEN, TK_ENDIF,
+	TK_READ, TK_WRITE, TK_RETURN, TK_CALL, TK_RECORD,
+	TK_ENDRECORD, TK_ELSE,
+	
+	// Operators
+	TK_ASSIGNOP, TK_PLUS, TK_MINUS, TK_MUL, TK_DIV,
+	TK_AND, TK_OR, TK_NOT, TK_LT, TK_LE, TK_EQ,
+	TK_GT, TK_GE, TK_NE,
+	
+	// Delimiters
+	TK_SQL, TK_SQR, TK_COMMA, TK_SEM, TK_COLON,
+	TK_DOT, TK_OP, TK_CL,
+	
+	// Complex tokens
+	TK_ID, TK_NUM, TK_RNUM, TK_FUNID, TK_RUID,
+	TK_FIELDID, TK_COMMENT,
+	
+	// // Special tokens, does not occur, but neccessary anyway
+	// EPSILON_TOKEN, DOLLAR_TOKEN
+} TokenType;
+
+TokenType mapping[] = {
+	TK_WITH, TK_PARAMETERS, TK_END, TK_WHILE, TK_UNION,
+	TK_ENDUNION, TK_DEFINETYPE, TK_AS, TK_TYPE, TK_MAIN,
+	TK_GLOBAL, TK_PARAMETER, TK_LIST, TK_INPUT, TK_OUTPUT,
+	TK_INT, TK_REAL, TK_ENDWHILE, TK_IF, TK_THEN, TK_ENDIF,
+	TK_READ, TK_WRITE, TK_RETURN, TK_CALL, TK_RECORD,
+	TK_ENDRECORD, TK_ELSE,
+	TK_ASSIGNOP, TK_PLUS, TK_MINUS, TK_MUL, TK_DIV,
+	TK_AND, TK_OR, TK_NOT, TK_LT, TK_LE, TK_EQ,
+	TK_GT, TK_GE, TK_NE,
+	TK_SQL, TK_SQR, TK_COMMA, TK_SEM, TK_COLON,
+	TK_DOT, TK_OP, TK_CL,
+	TK_ID, TK_NUM, TK_RNUM, TK_FUNID, TK_RUID,
+	TK_FIELDID, TK_COMMENT, 
+	
+	// EPSILON_TOKEN, DOLLAR_TOKEN // Not present
+};
+
+typedef struct twinBuffer
+{
+    char *buffer1;
+    char *buffer2;
+} twinBuffer;
+
+typedef struct Token
+{
+	char* lexeme;
+	TokenType type;
+	int line_no;
+
+} Token;
+
 // Define token mapping structure
 typedef struct {
 	const char* name;
